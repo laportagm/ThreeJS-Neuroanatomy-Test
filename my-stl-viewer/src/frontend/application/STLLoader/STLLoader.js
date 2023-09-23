602eb269673a264db93418bb7493ec3644057fa7
laportagm <laportagm@gmail.com>
2023-09-22 22:27:12 -0400

    Edit

--- my-stl-viewer/src/frontend/application/STLLoader/STLLoader.js
+++ my-stl-viewer/src/frontend/application/STLLoader/STLLoader.js
@@ -1,46 +1,26 @@
-// src/frontend/application/STLLoader/index.js
-
 import * as THREE from 'three';
-import { STLLoader } from 'three/examples/jsm/loaders/STLLoader.js';
+import { STLLoader as ThreeSTLLoader } from 'three/examples/jsm/loaders/STLLoader.js';
 
-export class STLModel {
-  constructor(path, scene, options = {}) {
-    this.path = path;
+class SimpleSTLLoader {
+  constructor(scene) {
     this.scene = scene;
-    this.loader = new STLLoader();
-    this.mesh = null;
-    this.defaultOptions = {
-      color: 0x00ff00,
-      position: { x: 0, y: 0, z: 0 },
-      // Add more default options here
-    };
-    this.options = { ...this.defaultOptions, ...options };
+    this.loader = new ThreeSTLLoader();
   }
 
-  load() {
+  loadModel(filename, position = { x: 0, y: 0, z: 0 }) {
     return new Promise((resolve, reject) => {
-      this.loader.load(this.path, (geometry) => {
-        const material = new THREE.MeshPhongMaterial({ color: this.options.color });
-        this.mesh = new THREE.Mesh(geometry, material);
-        this.mesh.position.set(this.options.position.x, this.options.position.y, this.options.position.z);
-        this.scene.add(this.mesh);
-        resolve(this.mesh);
-      }, null, reject);
+      this.loader.load(`assets/stl-files/${filename}`, (geometry) => {
+        const material = new THREE.MeshStandardMaterial({ color: 0x808080 });
+        const mesh = new THREE.Mesh(geometry, material);
+        mesh.position.set(position.x, position.y, position.z);
+
+        this.scene.add(mesh);
+        resolve(mesh);
+      }, undefined, (error) => {
+        reject(error);
+      });
     });
   }
-
-  updateOptions(newOptions) {
-    this.options = { ...this.options, ...newOptions };
-    if (this.mesh) {
-      if (newOptions.color) {
-        this.mesh.material.color.set(newOptions.color);
-      }
-      if (newOptions.position) {
-        this.mesh.position.set(newOptions.position.x, newOptions.position.y, newOptions.position.z);
-      }
-      // Update other properties here
-    }
-  }
-
-  // Add more methods for interactivity, annotations, etc.
 }
+
+export default SimpleSTLLoader;
